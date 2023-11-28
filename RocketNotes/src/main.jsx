34d492969741a1c6055729af1993e5724789d507
Fernando Rodrigues <fernando111oficina@gmail.com>
2023-11-29 00:40:06 +0100

    add

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from "styled-components"
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
+import { Details } from './assets/pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme = {theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
